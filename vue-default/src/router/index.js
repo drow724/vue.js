b4121d0819f67933bd775bef6f8c6b10507ad067
drow724 <drow724@naver.com>
2022-03-30 14:12:03 +0900

    Lazy Load (비동기 컴포넌트) 구성하기

--- vue-default/src/router/index.js
+++ vue-default/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ContactView from '../views/ContactView.vue'
+//import ContactView from '../views/ContactView.vue'
+//import AboutView from '../views/AboutView.vue'
 
 const routes = [
   {
@@ -8,6 +9,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   component: AboutView
+  // },
   {
     path: '/about',
     name: 'about',
@@ -19,7 +25,7 @@ const routes = [
   {
     path: '/contact',
     name: 'contact',
-    component: ContactView
+    component: () => import(/* webpackChunkName: "contact", webpackPrefetch:true */ '../views/ContactView.vue') //주석을 이용하여서 prefetch 부분 적용
   }
 ]
 

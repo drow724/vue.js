da90513bc29b0f53adee99a9c73f1b8046925170
drow724 <drow724@naver.com>
2022-03-31 14:45:15 +0900

    부모 컴포넌트에서 자식 컴포넌트의 데이터 값 동기화하기

--- vue-default/src/views/ChildComponentView.vue
+++ vue-default/src/views/ChildComponentView.vue
@@ -2,6 +2,7 @@
 <div>
     <button ref="child_btn" type="button" @click="childFunc">자식에 있는 클릭(alert 함수 호출)</button>
     <button type="button" @click="sendFromChild">자식에 있는 클릭(부모에게 영향주는 버튼)</button>
+    <button type="button" @click="changeData">자식에 있는 클릭(자신 데이타 변경)</button>
     <div> likes: {{likes}}</div>
     <div> isOk: {{isOk}}</div>
     <div> author: {{author.name}}</div>
@@ -54,6 +55,9 @@
             },
             sendFromChild() {
                 this.$emit('send-msg', this.msg);
+            },
+            changeData() {
+                this.msg = '자신이 변경한 메시지'
             }
         }
     }
--- vue-default/src/views/ExampleView.vue
+++ vue-default/src/views/ExampleView.vue
@@ -5,6 +5,7 @@
     <button type="button" @click="callChildBtnFunc">부모에 있는 클릭(자식 버튼 클릭)</button>
     <button type="button" @click="callChildFunc">부모에 있는 클릭(자식 함수 바로 호출)</button>
     <button type="button" @click="modifyChildMsg">부모에 있는 클릭(자식 메시지 변경)</button>
+    <button type="button" @click="showData">부모에 있는 클릭(자식 메시지 가져와서 보여주기)</button>
     <child-component-view :likes="23" :isOk="true" :commentIds="[1,5,2,3]" :author="author" ref="child_component" @send-msg="sendMessage" />
 </div>
 </template>
@@ -28,6 +29,11 @@ import ChildComponentView from './ChildComponentView';
                 author: {name:'홍길동', company:'회사이름'},
                 parentMsg: ''
             };
+        },
+        computed: {
+            msg() {
+                return this.$refs.child_component.msg;
+            }
         },
         setup() {
          
@@ -54,6 +60,9 @@ import ChildComponentView from './ChildComponentView';
             sendMessage(data){
                 alert(data);
                 this.parentMsg = data;
+            },
+            showData(){
+                alert(this.msg);
             }
         }
     }

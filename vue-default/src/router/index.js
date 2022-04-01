07e03bd2634086cfca94d533fa4a0b4d69e3352d
drow724 <drow724@naver.com>
2022-04-01 10:15:18 +0900

    컴포지션 API에서 Provide/Inject

--- vue-default/src/router/index.js
+++ vue-default/src/router/index.js
@@ -93,7 +93,12 @@ const routes = [
     path: '/composition4',
     name: 'Composition API4',
     component: () => import( /* webpackChunkName: "composition4" */ '../views/CompositionAPI4.vue')
-  }
+  },
+  {
+    path: '/composition_provide',
+    name: 'Composition API Provide',
+    component: () => import( /* webpackChunkName: "composition_provide" */ '../views/CompositionAPIProvide.vue')
+  },
 ]
 
 const router = createRouter({
